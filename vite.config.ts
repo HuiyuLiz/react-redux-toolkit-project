7aed8df1af6b7a49fc186f7b7c15dc8cc1923aaa
Huiyu Li <curlyuu@gmail.com>
2024-01-07 00:35:40 +0800

    chore: add git action

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-redux-toolkit-project/',
   plugins: [react()],
   resolve: {
     alias: {
